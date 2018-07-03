f3eafb01d75576f75b0feaa40891f0553ddec2ac
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-03 01:41:05 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 20160108015408) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.string   "artist_name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
